a48eb45267931b3b3967be9b2f7087e179850073
Ssuubi-Grace <muwongegrace2@gmail.com>
2025-02-06 18:27:25 +0300

    Add error message for alphabet input

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get("/api/classify-number", async (req, res) => {
   const number = parseInt(req.query.number);
 
   if (isNaN(number)) {
-    return res.status(400).json({ error: true });
+    return res.status(400).json({ error: true , message: "alphabet" });
   }
 
   // Fetching fun fact from Numbers API
